9602386e719785f533e260efee83958bd2c4343c
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-04-14 22:43:33 +0700

    * Randomize food position after eaten

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    public BoxCollider2D gridArea;
+
+    void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void RandomizePosition()
+    {
+        if (!gridArea) return;
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = Mathf.Round(Random.Range(bounds.min.x, bounds.max.x));
+        float y = Mathf.Round(Random.Range(bounds.min.y, bounds.max.y));
+
+        this.transform.position = new Vector3(x, y, 0f);
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var snake = other.gameObject.GetComponent<Snake>();
         if (snake != null)
         {
-            Destroy(this.gameObject);
+            RandomizePosition();
         }
     }
 }
